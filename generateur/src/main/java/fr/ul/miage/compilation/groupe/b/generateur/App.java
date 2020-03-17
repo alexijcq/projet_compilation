5eb1c28e68799e4edc36dd45901553e23c6219de
Alexi Jacquet <alexi.jacquet7@gmail.com>
2020-03-17 17:50:42 +0100

    initialisation projet

---
+++ generateur/src/main/java/fr/ul/miage/compilation/groupe/b/generateur/App.java
@@ -0,0 +1,13 @@
+package fr.ul.miage.compilation.groupe.b.generateur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
